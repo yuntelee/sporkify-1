30e7ec664551320879c0f30e96fc7789b6e7ad57
yuntelee <jyl2199@columbia.edu>
2025-08-17 21:30:43 -0400

    bug fix 2 gh pages install

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sporkify-1', // This is important for GitHub Pages
+  base: '/sporkify-1/', // This is important for GitHub Pages
   // server: {
   //   port: 3000,
   //   open: true
