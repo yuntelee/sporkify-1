283be60628df6539933d2e8eb45c9772342dac24
yuntelee <jyl2199@columbia.edu>
2025-08-17 21:27:51 -0400

    bug fix 1 vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/sporkify-1', // This is important for GitHub Pages
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  // server: {
+  //   port: 3000,
+  //   open: true
+  // },
+  // build: {
+  //   outDir: 'dist',
+  //   assetsDir: 'assets'
+  // }
 })
